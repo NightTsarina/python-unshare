e08161757b78ba0ea69566b0b776513c14333e36
martin.ferrari <martin.ferrari@localhost>
2010-05-18 21:46:02 +0000

    retab and proper clean target

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
+# vim: ts=4:sw=4:et:ai:sts=4
 from distutils.core import setup, Extension
 
 module1 = Extension('unshare', sources = ['unshare.c'])
 
-setup (name = 'Unshare',
-       version = '0.1',
-       description = 'Python bindings for the Linux unshare() syscall',
-       author = 'Martin Ferrari',
-       author_email = 'martin.ferrari@gmail.com',
-       url = 'http://code.google.com/p/python-unshare/',
-       ext_modules = [module1])
+setup(
+        name        = 'Unshare',
+        version     = '0.1',
+        description = 'Python bindings for the Linux unshare() syscall',
+        author      = 'Martin Ferrari',
+        author_email = 'martin.ferrari@gmail.com',
+        url         = 'http://code.google.com/p/python-unshare/',
+        license     = 'GPLv2',
+        ext_modules = [module1])
