79c2ffc3a49cfc44e5be5b9c003e57a3793c9101
martin.ferrari <martin.ferrari@localhost>
2010-05-18 21:51:43 +0000

    More metadata

--- setup.py
+++ setup.py
@@ -4,13 +4,22 @@
 from distutils.core import setup, Extension
 
 module1 = Extension('unshare', sources = ['unshare.c'])
+longdesc = '''This simple extension provides bindings to the Linux unshare() syscall, added in kernel version 2.6.16
+
+By using unshare(), new and interesting features of the Linux kernel can be exploited, such as:
+
+* Creating a new network name space (CLONE_NEWNET)
+* Creating a new file system mount name space (CLONE_NEWNS)
+* Reverting other features shared from clone()'''
 
 setup(
         name        = 'Unshare',
         version     = '0.1',
         description = 'Python bindings for the Linux unshare() syscall',
+        long_description = longdesc,
         author      = 'Martin Ferrari',
         author_email = 'martin.ferrari@gmail.com',
         url         = 'http://code.google.com/p/python-unshare/',
         license     = 'GPLv2',
+        platforms   = 'Linux',
         ext_modules = [module1])
