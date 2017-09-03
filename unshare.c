4f292226abdd38f267df38f6ecf792ad00312b03
Anders Blomdell <anders.blomdell@control.lth.se>
2017-09-03 12:34:10 +0200

    Logic cleanup. Added unbind function

--- unshare.c
+++ unshare.c
@@ -38,6 +38,8 @@ struct mapping {
     char *mask_name;
     char *format;
     int is_mounted;
+    int ns_old;
+    int ns_new;
 };
 
 #ifndef CLONE_NEWCGROUP
@@ -66,6 +68,7 @@ static struct mask_list_entry {
 
 static PyObject * _unshare(PyObject *self, PyObject *args, PyObject *keywds)
 {
+    PyObject *result = NULL;
     int ret;
 
     int v_flags = 0;
@@ -80,13 +83,13 @@ static PyObject * _unshare(PyObject *self, PyObject *args, PyObject *keywds)
     static char *kwlist[] = { "flags", "cgroup", "ipc", "mount", "net", "pid", "user", "uts", NULL };
     struct mapping mapping[] = {
         { NULL, kwlist[0], 0, NULL, NULL, 0 },
-        { &v_cgroup, kwlist[1], CLONE_NEWCGROUP, "CLONE_NEWCGROUP", "/proc/%d/ns/cgroup", 0 },
-        { &v_ipc, kwlist[2], CLONE_NEWIPC, "CLONE_NEWIPC", "/proc/%d/ns/ipc", 0 },
-        { &v_mount, kwlist[3], CLONE_NEWNS, "CLONE_NEWNS", "/proc/%d/ns/mnt", 0 },
-        { &v_net, kwlist[4], CLONE_NEWNET, "CLONE_NEWNET", "/proc/%d/ns/net", 0 },
-        { &v_pid, kwlist[5], CLONE_NEWPID, "CLONE_NEWPID", "/proc/%d/ns/pid", 0 },
-        { &v_user, kwlist[6], CLONE_NEWUSER, "CLONE_NEWUSER", "/proc/%d/ns/user", 0 },
-        { &v_uts, kwlist[7], CLONE_NEWUTS, "CLONE_NEWUTS", "/proc/%d/ns/uts", 0 },
+        { &v_cgroup, kwlist[1], CLONE_NEWCGROUP, "CLONE_NEWCGROUP", "/proc/%d/ns/cgroup", 0, -1, -1 },
+        { &v_ipc, kwlist[2], CLONE_NEWIPC, "CLONE_NEWIPC", "/proc/%d/ns/ipc", 0, -1, -1 },
+        { &v_mount, kwlist[3], CLONE_NEWNS, "CLONE_NEWNS", "/proc/%d/ns/mnt", 0, -1, -1 },
+        { &v_net, kwlist[4], CLONE_NEWNET, "CLONE_NEWNET", "/proc/%d/ns/net", 0, -1, -1 },
+        { &v_pid, kwlist[5], CLONE_NEWPID, "CLONE_NEWPID", "/proc/%d/ns/pid_for_children", 0, -1, -1 },
+        { &v_user, kwlist[6], CLONE_NEWUSER, "CLONE_NEWUSER", "/proc/%d/ns/user", 0, -1, -1 },
+        { &v_uts, kwlist[7], CLONE_NEWUTS, "CLONE_NEWUTS", "/proc/%d/ns/uts", 0, -1, -1 },
         { NULL, NULL, 0, NULL, NULL, 0 }
     };
 
@@ -95,10 +98,11 @@ static PyObject * _unshare(PyObject *self, PyObject *args, PyObject *keywds)
                                      &v_net, &v_pid, &v_user, &v_uts)) {
         return NULL;
     }
-    
+
     int flags_child = 0;
     int flags_parent = v_flags;
     pid_t my_pid = getpid();
+
     struct mapping *m;
     for ( m = mapping + 1 ; m->path ; m++) {
         if (*m->path != empty) {
@@ -110,7 +114,7 @@ static PyObject * _unshare(PyObject *self, PyObject *args, PyObject *keywds)
                 ret = stat(ns, &stat_buf);
                 if (ret == -1) {
                     PyErr_SetFromErrnoWithFilename(PyExc_OSError, ns);
-                    goto err_unbindable_namespace;
+                    goto err_file_missing;
                 }
                 flags_child |= m->mask;
                 flags_parent &= ~m->mask;
@@ -120,119 +124,217 @@ static PyObject * _unshare(PyObject *self, PyObject *args, PyObject *keywds)
             }
         }
     }
-        
+
+    int fd[2] = { -1, -1 };
     if (flags_child) {
-        int fd[2];
         ret = socketpair(AF_UNIX, SOCK_STREAM, 0, fd);
-        if(ret == -1)
-            return PyErr_SetFromErrno(PyExc_OSError);
+        if(ret == -1) {
+             PyErr_SetFromErrno(PyExc_OSError);
+             goto err_socketpair;
+        }
         pid_t pid = fork();
         if (pid < 0) {
-            close(fd[0]);
-            close(fd[1]);
-            return PyErr_SetFromErrno(PyExc_OSError);
+            PyErr_SetFromErrno(PyExc_OSError);
+            goto err_fork;
         } else if (pid == 0) {
             /* Child */
             close(fd[1]);
             unsigned char result = 0;
             ret = unshare(flags_child);
+            if (flags_child & CLONE_NEWPID) {
+                /* Force pid_for_children to appear */
+                pid_t pid = fork();
+                if (pid == 0) {
+                    exit(0);
+                }
+                waitpid(pid, NULL, 0);
+            }
             if (ret != 0) {
                 result = errno < 255 ? errno : EINVAL;
             }
             if (write(fd[0], &result, 1) != 1) {
-                close(fd[0]);
-                exit(1);
+                goto exit_1;
             }
             ret = read(fd[0], &result, 1);
             if (ret != 1 || result != 'Q') {
-                close(fd[0]);
-                exit(1);
+                goto exit_1;
             }
             close(fd[0]);
             exit(0);
+        exit_1:
+            close(fd[0]);
+            exit(1);
         } else {
             /* Parent */
             unsigned char result;
-            
+
             close(fd[0]);
+            fd[0] = -1;
             ret = read(fd[1], &result, 1);
             if (ret == -1) {
-                close(fd[1]);
-                return PyErr_SetFromErrno(PyExc_OSError);
+                PyErr_SetFromErrno(PyExc_OSError);
+                goto out;
             }
             if (result != 0) {
                 errno = result;
-                return PyErr_SetFromErrno(PyExc_OSError);
+                PyErr_SetFromErrno(PyExc_OSError);
+                goto out;
             }
             for ( m = mapping + 1 ; m->path ; m++) {;
                 if (*m->path != NULL && *m->path != empty) {
-                    
                     char ns[PATH_MAX];
+                    /* Save a restoration namespace in case of error */
+
+                    snprintf(ns, sizeof(ns), m->format, my_pid);
+                    m->ns_old = open(ns, O_RDONLY);
+                    if (m->ns_old == -1) {
+                        PyErr_SetFromErrnoWithFilename(PyExc_OSError, ns);
+                        goto err_ns_old_open;
+                    }
                     snprintf(ns, sizeof(ns), m->format, pid);
+                    m->ns_new = open(ns, O_RDONLY);
+                    if (m->ns_new == -1) {
+                        PyErr_SetFromErrnoWithFilename(PyExc_OSError, ns);
+                        goto err_ns_new_open;
+                    }
                     ret = mount(ns, *m->path, NULL, MS_BIND, NULL);
                     if (ret == -1) {
                         close(fd[1]);
                         PyErr_SetFromErrnoWithFilename(PyExc_OSError, *m->path);
-                        goto err_cleanup_mounts;
+                        goto err_ns_mount;
                     } else {
                         m->is_mounted = 1;
                     }
                 }
             }
-            for ( m = mapping + 1 ; m->path ; m++) {
-                if (flags_child & m->mask) {
-                    char ns[PATH_MAX];
-                    int fd;
-                    snprintf(ns, sizeof(ns), m->format, pid);
-                    fd = open(ns, O_RDONLY);
-                    if (fd < 0) {
-                        PyErr_SetFromErrnoWithFilename(PyExc_OSError, ns);
-                        goto err_open_ns_failed;
-                    }
-                    /* Temporarily unshare namespace before entering 
-                       child namespace (gdm's use of pam needs this) */
-                    ret = unshare(m->mask);
-                    if (ret == -1) {
-                        PyErr_SetFromErrnoWithFilename(PyExc_OSError, ns);
-                        goto err_unshare_ns_failed;
-                    }
-                    ret = setns(fd, m->mask);
-                    close(fd);
-                    if (ret == -1) {
-                        PyErr_SetFromErrnoWithFilename(PyExc_OSError, ns);
-                        goto err_setns_failed;
-                    }
-                }
-            }
-            
             if (write(fd[1], "Q", 1) != 1) {
-                close(fd[1]);
-                return PyErr_SetFromErrno(PyExc_OSError);
+                PyErr_SetFromErrno(PyExc_OSError);
+                goto err_child_terminate;
             }
-            close(fd[1]);
             waitpid(pid, NULL, 0);
         }
     }
-    ret = unshare(flags_parent);
-    if (ret == -1) {
-        /* If flags_child != 0 we are in serious trouble */
-        return PyErr_SetFromErrno(PyExc_OSError);
+    if (flags_parent & ~CLONE_NEWUSER) {
+        ret = unshare(flags_parent & ~CLONE_NEWUSER);
+        if (ret == -1) {
+            PyErr_SetFromErrno(PyExc_OSError);
+            goto err_unshare_failed;
+        }
     }
-    Py_RETURN_NONE;
+    for ( m = mapping + 1 ; m->path ; m++) {
+        if (m->ns_new != -1) {
+            ret = setns(m->ns_new, m->mask);
+            if (ret == -1) {
+                PyErr_SetFromErrno(PyExc_OSError);
+                goto err_setns_failed;
+            }
+        }
+    }
+    if (flags_parent & CLONE_NEWUSER) {
+        ret = unshare(CLONE_NEWUSER);
+        if (ret == -1) {
+            PyErr_SetFromErrno(PyExc_OSError);
+            goto err_unshare_newuser_failed;
+        }
+    }
+    if (flags_parent & CLONE_NEWPID) {
+        /* Force pid_for_children to appear */
+        pid_t pid = fork();
+        if (pid == 0) {
+            exit(0);
+        }
+        waitpid(pid, NULL, 0);
+    }
+    result = Py_None;
+    goto out;
 
-err_open_ns_failed:
-err_unshare_ns_failed:
+
+err_child_terminate:
+err_unshare_newuser_failed:
 err_setns_failed:
-err_cleanup_mounts:
+    /* restore old namespaces */
+    for ( m = mapping + 1 ; m->path ; m++) {
+        if (m->ns_old != -1) {
+            ret = setns(m->ns_old, m->mask);
+        }
+    }
+err_unshare_failed:
+err_ns_mount:
+err_ns_new_open:
+err_ns_old_open:
+    /* Remove (now invalid) ns mounts */
     for ( m = mapping + 1 ; m->path ; m++) {;
         if (m->is_mounted) {
             umount(*m->path);
             m->is_mounted = 0;
         }
     }
-    
-err_unbindable_namespace:
-    return NULL;
+err_fork:
+err_socketpair:
+err_file_missing:
+
+out:
+    for ( m = mapping + 1 ; m->path ; m++) {;
+        if (m->ns_old != -1) {
+            close(m->ns_old);
+            m->ns_old = -1;
+        }
+        if (m->ns_new != -1) {
+            close(m->ns_new);
+            m->ns_new = -1;
+        }
+    }
+    if (fd[0] != -1) { close(fd[0]); }
+    if (fd[1] != -1) { close(fd[1]); }
+    if (result == NULL) {
+        return NULL;
+    } else {
+        Py_RETURN_NONE;
+    }
+}
+
+static PyObject * _unbind(PyObject *self, PyObject *args) {
+    char *path;
+    int nstype, ret;
+    if (!PyArg_ParseTuple(args, "si", &path, &nstype))
+        return NULL;
+    pid_t pid = fork();
+    if (pid < 0) {
+        PyErr_SetFromErrno(PyExc_OSError);
+        return NULL;;
+    } else if (pid == 0) {
+        /* Child */
+        int fd;
+        fd = open(path, O_RDONLY);
+        if (fd == -1) {
+            exit(1);
+        }
+        ret = setns(fd, nstype);
+        if (ret == -1) {
+            close(fd);
+            exit(1);
+        }
+        close(fd);
+        exit(0);
+    } else {
+        /* Parent */
+        int status, res;
+        res = waitpid(pid, &status, 0);
+        if (res == -1) {
+            PyErr_SetFromErrno(PyExc_OSError);
+            return NULL;
+        }
+        if (status != 0) {
+            PyErr_Format(PyExc_OSError, "%s does not refer to namspace of kind %x", path, nstype);
+            return NULL;
+        }
+        res = umount(path);
+        if (res == -1) {
+            PyErr_SetFromErrnoWithFilename(PyExc_OSError, path);
+            return NULL;
+        }
+    }
+    Py_RETURN_NONE;
 }
 
 static PyObject * _setns(PyObject *self, PyObject *args) {
@@ -264,6 +366,12 @@ static PyMethodDef methods[] = {
      "  pid=PATH       save new pid namespace to PATH (CLONE_NEWPID)\n"
      "  user=PATH      save new user namespace to PATH (CLONE_NEWUSER)\n"
      "  uts=PATH       save new uts namespa<ce to PATH (CLONE_NEWUTS)\n"
+    },
+    {"unbind", _unbind, METH_VARARGS,
+     "unbind(path, nstype)\n\n"
+     "Unbind path from its current namespace.\n"
+     "  path is bound to a namespace.\n"
+     "  nstype specifies the type of namespace that should be unbound.\n"
     },
     {"setns", _setns, METH_VARARGS,
      "setns(fd, nstype)\n\n"
